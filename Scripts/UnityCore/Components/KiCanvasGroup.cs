3cbceb7947c0e18e5a002dc607e4b495929e393f
Kimicu <kitgunplay@gmail.com>
2023-09-27 19:10:52 +0300

    feat: KiCanvasGroup and update README.md

---
+++ Scripts/UnityCore/Components/KiCanvasGroup.cs
@@ -0,0 +1,29 @@
+﻿using KiUtility;
+using NaughtyAttributes;
+using UnityEngine;
+
+namespace KimicuUtility.Scripts.UnityCore.Components
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class KiCanvasGroup : MonoBehaviour
+    {
+        private CanvasGroup _canvasGroup;
+
+        private void OnValidate()
+        {
+            _canvasGroup ??= GetComponent<CanvasGroup>();
+        }
+
+        [Button("OFF")]
+        private void TurnOff()
+        {
+            _canvasGroup.Active(false);
+        }
+
+        [Button("ON")]
+        private void TurnOn()
+        {
+            _canvasGroup.Active(true);
+        }
+    }
+}
